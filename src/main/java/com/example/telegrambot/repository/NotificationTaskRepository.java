2a854560b380924fbfeef8c2aa18dc94afc26774
Александр <Alexander.popov.usurt@mail.ru>
2023-07-11 23:13:01 +0500

    Телеграм бот

--- src/main/java/com/example/telegrambot/repository/NotificationTaskRepository.java
+++ src/main/java/com/example/telegrambot/repository/NotificationTaskRepository.java
@@ -3,5 +3,5 @@ package com.example.telegrambot.repository;
 import com.example.telegrambot.entity.NotificationTask;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface NotificationTaskRepository extends JpaRepository<NotificationTask, Long> {
+public interface NotificationTaskRepository extends JpaRepository<NotificationTask,Long> {
 }
